cdc94493314cbb8b84422373a103a379ae8ee15a
Zamo <samyy_1614@hotmail.com>
2019-02-06 17:41:24 -0600

    Se agrego la opcion de guardar y eliminar de base de datos citas asi como control de formulario del modal

--- mod/agenda.php
+++ mod/agenda.php
@@ -3,14 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Agenda Dental Group</title>
-	
 	<link rel='stylesheet' type="text/css" href='../css/fullcalendar.css' />
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<link rel="shortcut icon" href="../img/iconos/icon.ico">
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/dental.css">
 	<link rel="stylesheet" type="text/css" href="../css/popup.css">
-
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<script src='../js/jquery.min-calendar.js'></script>
 	<script src='../js/moment.min.js'></script>
@@ -29,7 +27,7 @@
 			<div class="col">
 				
 			</div>
-			<div class="col-8">
+			<div class="col-9">
 				<div id="CalendarioWeb"></div>
 			</div>
 			<div class="col">
@@ -49,6 +47,7 @@
 			    	</button>
 			    </div>
 			    <div class="modal-body">
+			    	<input type="hidden" name="agenda" id="id_agenda" >
 					<label>Selecciona un paciente</label>
 					<?php $selectPacientes = $conexion->query($SQL);?>
 					<select name="paciente" id="pacientesS" data-placeholder="Seleciona un paciente..." class="chosen-select" required>
@@ -56,29 +55,27 @@
 						<?php while ($fila = $selectPacientes->fetch_array()) {
 						echo "<option value='".$fila[1]."'>" . $fila[0] . "</option>";
 						} ?>
-					</select><br>
+					</select><br><br>
 					<label>Fecha</label>
 					<input type="text" name="fecha" id="txtFecha"><br>
 					<label>Titulo</label>
 					<input type="text" name="titulo" id="txtTitulo"><br>
-					<label>Hora</label>
-					<input type="text" name="hora" id="txtHora">	<br>   
+					<label>Hora Inicio</label>
+					<input type="text" name="hora" id="txtHora">	<br>  
 					<label>Descripcion</label>   			
 					<textarea id="txtDescripcion" rows="3"></textarea><br>
 					<label>Color</label>
-					<input type="color" value="#ff0000" id="txtColor" name="">
+					<input type="color" value="#37c929" id="txtColor" name="">
 				</div>		    	
 		      	<div class="modal-footer">
 			      	<button type="button" id="btnGuardar" class="btn btn-success">Guardar</button>
 			      	<button type="button" class="btn btn-primary">Modificar</button>
-			        <button type="button" class="btn btn-danger" >Borrar</button>
+			        <button type="button" id="btnEliminar" class="btn btn-danger" >Borrar</button>
 		      	</div>
 	      	</div>
     	</div>
   	</div>
-
-  	<!-- Modal -->
-
+<!-- Modal -->
 
 <script src="../js/chosen.jquery.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
@@ -87,12 +84,14 @@
 	<script src="../js/mdb.min.js"></script>
 	<script src="../js/menu.js"></script>
 	<script src="../js/popup.js"></script>
+	<script src="../js/sweetalert.min.js"></script>
 	
 	<script>
-		  $(function() {
-	        $('.chosen-select').chosen();
-	        $('.chosen-select-deselect').chosen({ allow_single_deselect: true });
-	      });
+	$(function() {
+		$('.chosen-select').chosen();
+		$('.chosen-select-deselect').chosen({ allow_single_deselect: true });
+	});
+
 	$(document).ready(function(){
 			$('#CalendarioWeb').fullCalendar({
 				header:{
@@ -101,36 +100,120 @@
 				    right: 'month,agendaWeek,agendaDay'
 				},
 			dayClick:function(date,jsEvent,view){
+				$("#txtTitulo").val("");
+				$("#txtDescripcion").val("");
+				$("#txtHora").val("");
+				$('#tituloEvento').html("Agenda una Cita");
+				$("#txtColor").val("#37c929");
 				$("#EventoModal").modal();
 				$('#EventoModal').on('shown.bs.modal', function () {
 				  $('.chosen-select', this).chosen('destroy').chosen();
 				});
 				$("#pacientesS").val([]);
-				document.getElementById("txtFecha").value = date.format();
+				$("#txtFecha").val(date.format());
 			},
-					events:'../php/citas.php',
+			events:'../php/citas.php',
 
 			eventClick: function(calEvent,jsEvent,view){
 				$('#tituloEvento').html(calEvent.title);
-				document.getElementById("txtDescripcion").value = calEvent.descripcion;
+				$("#txtTitulo").val(calEvent.title);
+				$("#txtDescripcion").val(calEvent.descripcion);
+				$("#txtColor").val(calEvent.color);
+				FechaHora = calEvent.start._i.split(" ");
+				$("#txtFecha").val(FechaHora[0]);
+				$("#txtHora").val(FechaHora[1]);				
 				$("#EventoModal").modal();
+				$('#EventoModal').on('shown.bs.modal', function () {
+				  $('.chosen-select', this).chosen('destroy').chosen();				  
+				});
+				$("#id_agenda").val(calEvent.id_agenda);
+				seleccionarPaciente(calEvent.id_paciente);
 			}
 		});
 	});
 
+	function seleccionarPaciente(id){
+		var select=document.getElementById("pacientesS");
+		for(var i=1;i<select.length;i++){
+			if(select.options[i].value==id){
+				select.selectedIndex=i;
+			}
+		}
+	};
+	
+	var NuevoEvento;
+
 	$('#btnGuardar').click(function(){
-		var NuevoEvento = {
-			title: $('#txtTitulo').val(),
-			start: $('#txtFecha').val()+" "+$('#txtHora').val(),
-			color: $('#txtColor').val(),
-			descripcion: $('#txtDescripcion').val(),
-			textColor: "#FFFFFF"
-		};
-		$('#CalendarioWeb').fullCalendar('renderEvent',NuevoEvento);
-		$('#EventoModal').modal('toggle');
+		RecolectarDatosGUI();
+		EnviarInformacion('agregar',NuevoEvento,"¡Cita Agendada!","Todo listo para recibir a su paciente");
 	});
 
+	$('#btnEliminar').click(function(){
+		swal({
+			  title: "Borrar Cita?",
+			  text: "Estas a punto de borrar una cita esto es verdad ?",
+			  icon: "warning",
+			  buttons: true,
+			  dangerMode: true,
+			})
+			.then((willDelete) => {
+			  if (willDelete) {
+			    RecolectarDatosGUI();
+				EnviarInformacion('eliminar',NuevoEvento,"¡Cita Borrada!","Ya no te preocupes por este paciente ya no vendra");
+			  } else {
+			    swal({
+					icon: "info",
+					title: "Todo Tranquilo",
+					text: "No te preoucupes no borramos la cita",
+					button: "Entiendo",
+					timer: 3000,
+				});
+			  }
+			});
 		
+	});
+
+	function RecolectarDatosGUI(){
+		NuevoEvento = {
+			id_paciente: $('#pacientesS').val(),
+			title: $('#txtTitulo').val(),
+			start: $('#txtFecha').val()+" "+$('#txtHora').val(),
+			end: $('#txtFecha').val()+" "+$('#txtHora').val(),
+			color: $('#txtColor').val(),
+			descripcion: $('#txtDescripcion').val(),
+			textColor: "#000000"
+		};
+	};
+
+	function EnviarInformacion(accion,objEvento,titulo,mensaje){
+		$.ajax({
+			type: 'POST',
+			url: '../php/citas.php?accion='+accion,
+			data: objEvento,
+			success: function(msg){
+				if(msg){
+					$('#CalendarioWeb').fullCalendar('refetchEvents');
+					$('#EventoModal').modal('toggle');
+					swal({
+						icon: "success",
+						title: titulo,
+						text: mensaje,
+						button: "Ok",
+						timer: 3000,
+					});
+				}
+			},
+			error:function(){
+				swal({
+					icon: "error",
+					title: "¡Tenemos un error!",
+					text: "Ocurrio un problema con la cita",
+					button: "Entiendo",
+					timer: 5000,
+				});
+			}
+		});
+	};
 	</script>
 </body>
 </html>
--- php/citas.php
+++ php/citas.php
@@ -1,11 +1,34 @@
 <?php
 header('Content-Type: application/json');
+
+
+$accion = (isset($_GET['accion']))?$_GET['accion']:'leer';
 include ("conexion.php");
+switch ($accion) {
+	case 'agregar':
+		$SentenciaSQL = "INSERT INTO Agenda(id_paciente,title,descripcion,start,end,color,textColor) VALUES (".$_POST['id_paciente'].",'".$_POST['title']."','".$_POST['descripcion']."','".$_POST['start']."','".$_POST['end']."','".$_POST['color']."','".$_POST['textColor']."')";
+		$respuesta =  $conexion->query($SentenciaSQL);
+		echo json_encode($respuesta);
+		break;
+	case 'modificar':
+
+		break;
+	case 'eliminar':
+		$res = $conexion->query("SELECT id_agenda FROM Agenda WHERE id_paciente =".$_POST['id_paciente']." and start LIKE '%".$_POST['start']."%';");
+		$f = $res->fetch_assoc();
+		if (isset($f['id_agenda'])) {
+			$eliminarSQL = "DELETE FROM Agenda WHERE id_agenda = " . $f['id_agenda'] . ";";
+			$respuesta =  $conexion->query($eliminarSQL);
+			echo json_encode($respuesta);
+		}
+		break;
+	default:
 		$SQL = "SELECT * FROM Agenda;";
 		$selectCita = $conexion->query($SQL);
-
-while ($fila = $selectCita->fetch_assoc()){
-	$rows[]=$fila;
-}
-echo json_encode($rows);
+		while ($fila = $selectCita->fetch_assoc()){
+			$rows[]=$fila;
+		}
+		echo json_encode($rows);
+		break;
+}	
 ?>
